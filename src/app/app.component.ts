124c08b90743eaf6d0c0dfc7eda9ef4133c0458f
Felix Ehuan <fnixro2@gmail.com>
2018-03-13 00:32:40 -0600

    Newsletter contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import {InformacionService} from "./informacion.service";
 import {Http} from "@angular/http";
+import {AngularFireDatabase} from "angularfire2/database";
+import {Observable} from "rxjs/Observable";
+import * as firebase from 'firebase/app';
+import { Validators,FormGroup } from '@angular/forms';
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,12 +18,19 @@ export class AppComponent {
   public team:any=[];
   public habilidades:any=[];
   public generales:any=[];
-  constructor(public _is:InformacionService, public http:Http){
+  public prices:any=[];
+  public email2:string;
+  public clientes:any=[];
+  date:any;
+  constructor(public _is:InformacionService, public http:Http, private afDB:AngularFireDatabase){
     this.home_data();
     this.team_data();
     this.skills();
     this.general();
-  }
+    this.prices_data();
+    this.clientes_data();
+    this.date= new Date().getFullYear();
+}
 
   home_data(){
     this.http.get("https://softlistig-cb3a2.firebaseio.com/header.json").subscribe(data=>{
@@ -26,11 +38,22 @@ export class AppComponent {
         this.header.splice(0,1);
 });
   }
+  clientes_data(){
+    this.http.get("https://softlistig-cb3a2.firebaseio.com/clientes.json").subscribe(data=>{
+      this.clientes=data.json();
+});
+  }
 
   team_data(){
 this.http.get("https://softlistig-cb3a2.firebaseio.com/team.json").subscribe(data=>{
   this.team=data.json();
     this.team.splice(0,1);
+});
+  }
+  prices_data(){
+this.http.get("https://softlistig-cb3a2.firebaseio.com/price.json").subscribe(data=>{
+  this.prices=data.json();
+  console.log(this.prices);
 });
   }
 
@@ -47,6 +70,61 @@ this.http.get("https://softlistig-cb3a2.firebaseio.com/team.json").subscribe(dat
       console.log("Generales: ",this.generales);
         this.generales.splice(0,1);
     });
-
   }
+  subscribe_news(f: NgForm){
+     console.log(f.value);
+      let news:newsletter={};
+      news.email=f.value.email;
+let aux = news.email.indexOf("@");
+    console.log("aux: ",aux);
+    let key =news.email;
+    key=news.email.substring(0,aux);
+    console.log(key);
+    this.afDB.object(`/newsletter/${key}`).update(news).then(()=>{
+      console.log("Listo")
+    }).catch(()=>{
+      console.log("Hubo un error, intente nuevamente");
+    });
+}
+send_comments(f: NgForm){
+   console.log(f.value);
+    let comment:comments={};
+    comment.comentario=f.value.comment;
+    comment.email=f.value.email;
+    comment.nombre=f.value.name;
+    comment.key=Date.now().valueOf();
+    if(!comment.nombre && !comment.email){
+
+    } else{
+      console.log(comment);
+      this.afDB.object(`/contactos/${comment.key}`).update(comment).then(()=>{
+        console.log("Listo")
+      }).catch((err)=>{
+        console.log("Hubo un error, intente nuevamente",err);
+      });
+    }
+    //news.email=f.value.email;
+
+// let aux = news.email.indexOf("@");
+//   console.log("aux: ",aux);
+//   let key =news.email;
+//   key=news.email.substring(0,aux);
+//   console.log(key);
+  // this.afDB.object(`/newsletter/${key}`).update(news).then(()=>{
+  //   console.log("Listo")
+  // }).catch(()=>{
+  //   console.log("Hubo un error, intente nuevamente");
+  // });
+}
+
+}
+interface newsletter {
+  email?:string;
+}
+
+interface comments {
+  nombre?:string;
+  comentario?:string;
+  email?:string;
+  key?:number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,38 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { ServiceComponent } from './components/service/service.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
-import { AboutComponent } from './components/about/about.component';
-import { ClientsFirstComponent } from './components/clients-first/clients-first.component';
-import { ClientsSecondComponent } from './components/clients-second/clients-second.component';
-import { PriceComponent } from './components/price/price.component';
-import { NewsletterComponent } from './components/newsletter/newsletter.component';
-import { ContactComponent } from './components/contact/contact.component';
 import {InformacionService} from "./informacion.service";
 import {HttpModule} from "@angular/http";
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import {AngularFireDatabase,AngularFireDatabaseModule} from "angularfire2/database";
+import { FormsModule } from '@angular/forms';
+var config = {
+    apiKey: "AIzaSyALrjXcphw3y1olVUAuxG4m2DGFyiNfGdc",
+    authDomain: "softlistig-cb3a2.firebaseapp.com",
+    databaseURL: "https://softlistig-cb3a2.firebaseio.com",
+    projectId: "softlistig-cb3a2",
+    storageBucket: "softlistig-cb3a2.appspot.com",
+    messagingSenderId: "428317011541"
+  };
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    ServiceComponent,
-    PortfolioComponent,
-    AboutComponent,
-    ClientsFirstComponent,
-    ClientsSecondComponent,
-    PriceComponent,
-    NewsletterComponent,
-    ContactComponent
-  ],
+      ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(config),
+    FormsModule
   ],
-  providers: [InformacionService],
+  providers: [InformacionService,
+    AngularFireDatabase,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
